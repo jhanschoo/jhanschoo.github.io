3faf5a17d2682d2ee4dacae491442119b710dd36
Johannes Choo <jhanschoo@gmail.com>
2025-06-11 15:29:30 +0800

    fix: correct math rendering bug, update lockfile

--- remark-plugins/remark-add-astro-plain-text.ts
+++ remark-plugins/remark-add-astro-plain-text.ts
@@ -1,10 +1,10 @@
-import { toString } from 'mdast-util-to-string';
-import type { RemarkPlugin } from '@astrojs/markdown-remark'; 
+import { toString } from "mdast-util-to-string";
+import type { RemarkPlugin } from "@astrojs/markdown-remark";
 
 const remarkAddAstroPlainText: RemarkPlugin = () => (tree, file) => {
   if (file.data.astro?.frontmatter) {
     file.data.astro.frontmatter.plainText = toString(tree);
   }
-}
+};
 
 export default remarkAddAstroPlainText;
